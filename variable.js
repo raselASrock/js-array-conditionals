598e4252f8eb9cebb5857ad188b96c58ed81199c
raselASrock <mdraselm413@gmail.com>
2022-08-01 00:58:39 +0600

    second-commit

--- variable.js
+++ variable.js
@@ -1,2 +1,24 @@
+// number type variable
 var sunglassPrice = 120;
+// string type variable
 var specialOneName = "Ariana Grande";
+// booliean type variable
+var issingle = true;
+// two types of number = 1. Integer: (126, 278); 2. Float: (12.56, 4.16);
+// variable convenction rules: best practise
+    // * 1. Single Word
+    // * 2. No gap or space
+    // * 3. No keyword or quoto
+    // * 4. Not start with a number
+    // * 5. Not contain dash
+    // * 6. Should use camelCase
+
+// Math operations: +, -, *, /;
+// Shorthand: +=, -=, *=, /=, ++, --;
+
+// Convart:
+    // parseInt: String to Intiger;
+    // parseFloat: String to float or deci,al number;
+
+// toFixed 
+// Reminder %
