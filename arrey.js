03ba08543835eac4cabbd13674eea5089107fd1f
raselASrock <mdraselm413@gmail.com>
2022-08-01 02:10:42 +0600

    arrey-added

---
+++ arrey.js
@@ -0,0 +1,6 @@
+var friendsAge = [21, 11, 25, 15, 6, 18];
+var picnicFee = [5000, 1500, 8000, 150];
+var heroine = ["mahi", "opu", "purnima"];
+var vowels = ["a", "e", "i", "o", "u"];
+console.log(vowels);
+console.log(friendsAge.length);
