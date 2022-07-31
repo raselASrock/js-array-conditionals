2b3bb9c4141c2880d46508bb98537412f239ffb9
raselASrock <mdraselm413@gmail.com>
2022-08-01 02:37:22 +0600

    index-added

---
+++ index.js
@@ -0,0 +1,10 @@
+var numbers = [15, 45, 58, 49, 94, 73];
+console.log(numbers);
+console.log(numbers[4]);
+var element = numbers[2];
+console.log(element);
+numbers[1] = 77;
+numbers[5] = 512;
+console.log(numbers);
+var positionIndex = numbers.indexOf(94);
+console.log(positionIndex); 
