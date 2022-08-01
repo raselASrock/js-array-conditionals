2f389d555f23001bb0b8facacf064148a569162e
raselASrock <mdraselm413@gmail.com>
2022-08-01 20:21:38 +0600

    multiple-conditions added

---
+++ multiple-conditions.js
@@ -0,0 +1,14 @@
+var isGraduated = true;
+var salary = 750000;
+var cars = 1;
+if(isGraduated == true && salary > 60000 && cars>= 1){
+    console.log("Eso biye kore feli")
+}
+else{
+    console.log("Kopale biya Nai")
+}
+
+
+var isGraduated = true;
+var salary = 750000;
+var cars = 1;
