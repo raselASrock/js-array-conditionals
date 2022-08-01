8b1046be6acd92b1b0e315e11e0143f81f2658c5
raselASrock <mdraselm413@gmail.com>
2022-08-01 20:35:25 +0600

    multi-stage-condition added

---
+++ multi-stage-conditions.js
@@ -0,0 +1,16 @@
+var money = 10;
+var danishPrice = 45;
+var battarBone = 35;
+var toastBiscut = 20;
+if(danishPrice < money){
+    console.log("Give Me A Danish")
+}
+else if(battarBone < money){
+    console.log("Give Me A BattarBone")
+}
+else if(toastBiscut < money){
+    console.log("Give Me A ToastBiskut")
+}
+else{
+    console.log("Cha Dau To Mama")
+}
