4529037f0152b65af265ef4b3312445073149417
raselASrock <mdraselm413@gmail.com>
2022-08-01 18:28:59 +0600

    push-pop added

---
+++ push-pop.js
@@ -0,0 +1,9 @@
+var numbers = [78, 45, 96, 82, 19];
+// numbers.push(63, 18, 372);
+numbers.pop();
+
+console.log(numbers);
+
+var friends =["balam", "rofiq","mustakim", "nobin" ];
+friends.push("kishor", "sazal", "rafsan");
+console.log(friends);
