c22b06c3533a7c7aaa053aebb2b8430b3ae09404
raselASrock <mdraselm413@gmail.com>
2022-08-01 19:50:12 +0600

    conditional added

---
+++ conditional.js
@@ -0,0 +1,18 @@
+var iphonePrice = 78000;
+var myBudget = 95000;
+/** 
+* if iphone price is less than my budget then i can buy an iphone
+
+* if (condition){
+        will execute if the condition is true
+}
+*
+**/ 
+if (iphonePrice < myBudget){
+    console.log("iphone kine futani marbo");
+}
+var chickenPrice = 500;
+var myMoney = 50;
+if(chickenPrice >= myMoney){
+    console.log("Murgir Raan Khabo")
+}
